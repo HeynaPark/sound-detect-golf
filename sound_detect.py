26e66a9291534795dc3393c8021458f2f8b69ab6
hnpark <hnpark@4dreplay.com>
2023-07-25 10:39:00 +0900

    add denoising func

--- sound_detect.py
+++ sound_detect.py
@@ -4,6 +4,39 @@ import moviepy.editor as mp
 import time
 from moviepy.editor import VideoFileClip
 import matplotlib.pyplot as plt
+import noisereduce as nr
+import pywt
+from scipy.io import wavfile
+import os
+
+
+def convert_mp4_to_wav_folder():
+    cur_dir = os.getcwd()
+
+    input_folder = cur_dir
+
+    if not os.path.exists(input_folder):
+        os.makedirs(input_folder)
+
+    # List all files in the input folder
+    files = os.listdir(input_folder)
+
+    for file in files:
+        # Check if the file is an mp4 file
+        if file.lower().endswith(".mp4"):
+            # Load the mp4 file using moviepy
+            mp4_file_path = os.path.join(input_folder, file)
+            video_clip = VideoFileClip(mp4_file_path)
+
+            # Extract the audio from the video clip
+            audio = video_clip.audio
+
+            # Create the output wav file path
+            wav_file_path = os.path.join(
+                input_folder, os.path.splitext(file)[0] + ".wav")
+
+            # Save the audio to wav format
+            audio.write_audiofile(wav_file_path)
 
 
 def convert_mp4_to_wav(mp4_file_path, wav_file_path):
@@ -31,40 +64,78 @@ def detect_golf_ball_strike(audio_data, sample_rate, target_freq_range, threshol
         return False
 
 
-def detect_impact_sounds(audio_file_path, threshold_db=-30, min_duration=0.3):
+def denoise_pywt(raw_data):
+
+    coeffs = pywt.wavedec(raw_data, 'db4', level=5)
+    noise_estimation = np.median(np.abs(coeffs[0]))/0.6745
+
+    threshold = 2*noise_estimation
+
+    coeffs_denoised = [pywt.threshold(
+        c, threshold, mode='soft') for c in coeffs]
+
+    denoised_data = pywt.waverec(coeffs_denoised, 'db4')
+
+    return denoised_data
+
+
+def denoise_nr(audio_file_path):
+
+    rate, data = wavfile.read(audio_file_path)
+    audio_data_denoised = nr.reduce_noise(y=data, sr=rate)
+
+    return audio_data_denoised
+
+
+def detect_impact_sounds(audio_file_path, threshold_db=-30):
     print("threshold_db", threshold_db)
     audio_data, sample_rate = librosa.load(audio_file_path, sr=None)
-    print("sample rate", sample_rate)
+
+    # denoising
+    audio_data_denoised = audio_data
+    # audio_data_denoised = denoise_pywt(audio_data)
+    # audio_data_denoised = denoise_nr(audio_file_path)
+
+    # print("sample rate", sample_rate)
 
     audio_duration = librosa.get_duration(y=audio_data, sr=sample_rate)
-    amplitude_db = librosa.amplitude_to_db(audio_data)
+    amplitude_db = librosa.amplitude_to_db(audio_data_denoised)
+    # amplitude_db = librosa.amplitude_to_db(audio_data)
 
+    max_duration = 1
+    min_duration = 0.1
     impact_time_points = []
-    start_time = 0
     for i in range(1, len(amplitude_db)):
-        if amplitude_db[i] >= threshold_db:  # 타격음 발생
+        if amplitude_db[i] >= threshold_db:  # Impact sound detected
+            print('detected something')
             if not impact_time_points:
+                # Start of a new impact sound
                 start_time = i / sample_rate
-                print("start_time", start_time)
+                print("Start time:", start_time)
             impact_time_points.append(i / sample_rate)
-
-        else:       # 타격음 끝남
+        else:  # Sound below threshold, impact sound ends
             if impact_time_points:
-                print("있었는데")
-                duration = impact_time_points[-1] - start_time
-                print(f"duration : {duration:.5f}")
-                if duration >= min_duration:
+                # Calculate the duration of the impact sound
+                end_time = (i - 1) / sample_rate
+                print("end time", end_time)
+                duration = end_time - start_time
+                # duration = impact_time_points[-1] - start_time
+                print(f"Duration: {duration:.5f} seconds")
+                if min_duration < duration < max_duration:
                     print(
-                        f"Impact sound detected at {start_time:.2f} seconds, Duration: {duration:.2f} seconds")
+                        f">>>>>>>>>>>Impact sound detected at {start_time:.2f} seconds, Duration: {duration:.2f} seconds")
                 impact_time_points = []
 
-    spectrogram = librosa.stft(audio_data)
+    spectrogram = librosa.stft(audio_data_denoised)
+    # spectrogram = librosa.stft(audio_data)
 
     # Plot both waveform and spectrogram
-    plt.figure(figsize=(12, 8))
+    plt.figure(figsize=(20, 10))
+    # plt.figure(figsize=(12, 8))
 
     plt.subplot(2, 1, 1)
-    librosa.display.waveshow(audio_data, sr=sample_rate)
+    librosa.display.waveshow(audio_data_denoised, sr=sample_rate)
+    # librosa.display.waveshow(audio_data, sr=sample_rate)
     plt.xlabel("Time (seconds)")
     plt.ylabel("Amplitude")
     plt.title(
@@ -73,23 +144,13 @@ def detect_impact_sounds(audio_file_path, threshold_db=-30, min_duration=0.3):
     plt.subplot(2, 1, 2)
     librosa.display.specshow(librosa.amplitude_to_db(
         spectrogram), sr=sample_rate, x_axis='time', y_axis='hz')
-    plt.colorbar(format='%+2.0f dB')
+    # plt.colorbar(format='%+2.0f dB')
     plt.xlabel("Time (seconds)")
     plt.ylabel("Frequency (Hz)")
     plt.title("Spectrogram")
 
     plt.tight_layout()
     plt.show()
-
-    # Plot the waveform
-    # plt.figure(figsize=(10, 4))
-    # librosa.display.waveshow(audio_data, sr=sample_rate)
-    # plt.xlabel("Time (seconds)")
-    # plt.ylabel("Amplitude")
-    # plt.title(
-    #     f"Waveform of Audio File: {audio_file_path}\nDuration: {audio_duration:.2f} seconds")
-    # plt.tight_layout()
-    # plt.show()
 
     time_data = np.arange(len(audio_data)) / float(sample_rate)
     whole_time = len(audio_data) / float(sample_rate)
@@ -119,7 +180,11 @@ def plot_waveform(audio_file_path):
 
 
 if __name__ == "__main__":
-    audio_file_path = "short_audio.wav"  # Replace with the path to your audio file
+    # Replace with the path to your audio file
+    audio_file_path = "data/video_timeCut.wav"
+    # audio_file_path = "data/video_470_508_timeCut.wav"
+    # audio_file_path = "short_audio.wav"  # Replace with the path to your audio file
     # plot_waveform(audio_file_path)
 
-    detect_impact_sounds(audio_file_path, threshold_db=-2, min_duration=0.1)
+    detect_impact_sounds(audio_file_path, threshold_db=-2)
+    # convert_mp4_to_wav_folder()
